dfd06f52c4ab4bb5d934c3bf95ff95b8619c1d94
woclass <git@wo-class.cn>
2023-01-19 10:35:10 +0800

    rename NVDA_ => BOY_IMPL_

--- nvda.h
+++ nvda.h
@@ -1,45 +1,37 @@
 ﻿#pragma once
 #include <winerror.h>
 
-#define NVDA_EXPORTS
-#ifdef NVDA_EXPORTS
-#define NVDA_API __declspec(dllexport)
+#define BOY_IMPL_EXPORTS
+#ifdef BOY_IMPL_EXPORTS
+#define BOY_IMPL_API __declspec(dllexport)
 #else
-#define NVDA_API __declspec(dllimport)
+#define BOY_IMPL_API __declspec(dllimport)
 #endif
 typedef unsigned long error_status_t;
 
-
-#ifdef __cplusplus
-extern "C" {
-#endif // def __cplusplus
 
 /**
  * @brief 检查读屏器是否在运行
  * @return 错误码
 */
-NVDA_API error_status_t __stdcall testIfRunning_impl();
+BOY_IMPL_API error_status_t __stdcall testIfRunning_impl();
 
 /**
  * @brief 向读屏器输出文本
  * @param text 待输出的文本
  * @return 错误码
 */
-NVDA_API error_status_t __stdcall speakText_impl(const wchar_t* text);
+BOY_IMPL_API error_status_t __stdcall speakText_impl(const wchar_t* text);
 
 /**
  * @brief 静音当前输出
  * @return 错误码
 */
-NVDA_API error_status_t __stdcall cancelSpeech_impl();
+BOY_IMPL_API error_status_t __stdcall cancelSpeech_impl();
 
 /**
  * @brief 【暂不支持】输出盲文
  * @param message 待输出的文本
  * @return 错误码
 */
-NVDA_API error_status_t __stdcall brailleMessage_impl(const wchar_t* message);
-
-#ifdef __cplusplus
-}
-#endif // def __cplusplus
+BOY_IMPL_API error_status_t __stdcall brailleMessage_impl(const wchar_t* message);
