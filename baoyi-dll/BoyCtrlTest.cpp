0a6be566687b16ed729f5d24cac19217a1ee6e51
woclass <git@wo-class.cn>
2023-01-20 21:32:35 +0800

    BoyCtrlTest.cpp 添加注释

--- baoyi-dll/BoyCtrlTest.cpp
+++ baoyi-dll/BoyCtrlTest.cpp
@@ -8,13 +8,23 @@
 using namespace std;
 
 
+/**
+ * @brief 朗读完成回调函数
+ * @param reason 回调原因。
+ *  1=朗读完成；
+ *  2=被新的朗读打断；
+ *  3=被停止调用打断
+ */
 void __stdcall speakCompleteCallback(int reason)
 {
     MessageBeep(0xffffffff);
 }
 
+/// 主函数
 int main()
 {
+    // 加载保益 DLL...
+    // 注意：DLL 需要放在可被搜索到的路径中，一般和主程序放在一起。
 #ifndef BOYCTRLTEST_X64
     auto dllHandle = LoadLibrary(L"BoyCtrl.dll");
 #else
@@ -29,6 +39,9 @@ int main()
 #endif
         return 1;
     }
+
+    // 开始加载 DLL 的导出函数...
+    /// 初始化接口
     auto initFunc = (decltype(BoyCtrlInitialize) *)GetProcAddress(dllHandle, "BoyCtrlInitialize");
     if (!initFunc)
     {
@@ -36,6 +49,7 @@ int main()
         FreeLibrary(dllHandle);
         return 1;
     }
+    /// 退出清理
     auto uninitFunc = (decltype(BoyCtrlUninitialize) *)GetProcAddress(dllHandle, "BoyCtrlUninitialize");
     if (!uninitFunc)
     {
@@ -43,6 +57,7 @@ int main()
         FreeLibrary(dllHandle);
         return 1;
     }
+    // 初始化接口，并输出日志
     auto err = initFunc(L"D:\\boyCtrl.log");
     if (err != e_bcerr_success)
     {
@@ -52,6 +67,7 @@ int main()
     }
     cout << "ready" << endl;
 
+    /// 朗读文本
     auto speakFunc = (decltype(BoyCtrlSpeak) *)GetProcAddress(dllHandle, "BoyCtrlSpeak");
     if (!speakFunc)
     {
@@ -68,6 +84,7 @@ int main()
         FreeLibrary(dllHandle);
         return 1;
     }
+    /// 停止朗读
     auto stopFunc = (decltype(BoyCtrlStopSpeaking) *)GetProcAddress(dllHandle, "BoyCtrlStopSpeaking");
     if (!stopFunc)
     {
@@ -76,10 +93,15 @@ int main()
         FreeLibrary(dllHandle);
         return 1;
     }
+    // 函数加载完毕。
+    
+
+    // 开始测试接口
     for (int i = 1; i <= 4; ++i)
     {
         cout << i << " Press <Enter> to speak" << endl;
         getchar();
+
         if (i < 3)
         {
             wostringstream oss;
@@ -87,14 +109,22 @@ int main()
             err = speakFunc(oss.str().c_str(), true, false, true, speakCompleteCallback);
         }
         else
+        {
             err = speakU8Func(u8"朗读字符串使用UTF-8", true, false, true, speakCompleteCallback);
+        }
+
         if (err != e_bcerr_success)
         {
             cerr << "error = " << err << endl;
         }
     }
+
+    // 程序退出，开始清理工作...
     uninitFunc();
     FreeLibrary(dllHandle);
+
     cout << "Press <Enter> to exit" << endl;
     getchar();
+
+    return 0;
 }
