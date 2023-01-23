85144fd7af5fee04ec49424137f60cd532323880
woclass <git@wo-class.cn>
2023-01-23 22:58:08 +0800

    NVDLL_IMPL RENAME

--- nvdll.h
+++ nvdll.h
@@ -1,17 +1,19 @@
 ﻿#pragma once
+#ifndef __NVDLL_H
+#define __NVDLL_H
 #include <winerror.h>
 
-#ifdef BOY_IMPL_IMPORTS
-#define BOY_IMPL_API __declspec(dllimport)
+#ifdef NVDLL_IMPL_IMPORTS
+#define NVDLL_IMPL_API __declspec(dllimport)
 #else // 导出函数
 #ifdef _DEBUG
 /// 调试模式时，导出实现 impl 函数
-#define BOY_IMPL_API __declspec(dllexport)
+#define NVDLL_IMPL_API __declspec(dllexport)
 #else // REL mode
 /// 发布模式时，不导出
-#define BOY_IMPL_API
+#define NVDLL_IMPL_API
 #endif // def _DEBUG
-#endif // def BOY_IMPL_IMPORTS
+#endif // def NVDLL_IMPL_IMPORTS
 
 /// 标准的错误返回值
 typedef unsigned long error_status_t;
@@ -20,34 +22,35 @@ typedef unsigned long error_status_t;
 
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif // __cplusplus
 
 /**
  * @brief 检查读屏器是否在运行
  * @return 错误码
 */
-BOY_IMPL_API WIN_RPC_RET testIfRunning_impl();
+NVDLL_IMPL_API WIN_RPC_RET testIfRunning_impl();
 
 /**
  * @brief 向读屏器输出文本
  * @param text 待输出的文本
  * @return 错误码
 */
-BOY_IMPL_API WIN_RPC_RET speakText_impl(const wchar_t* text);
+NVDLL_IMPL_API WIN_RPC_RET speakText_impl(const wchar_t* text);
 
 /**
  * @brief 静音当前输出
  * @return 错误码
 */
-BOY_IMPL_API WIN_RPC_RET cancelSpeech_impl();
+NVDLL_IMPL_API WIN_RPC_RET cancelSpeech_impl();
 
 /**
  * @brief 【暂不支持】输出盲文
  * @param message 待输出的文本
  * @return 错误码
 */
-BOY_IMPL_API WIN_RPC_RET brailleMessage_impl(const wchar_t* message);
+NVDLL_IMPL_API WIN_RPC_RET brailleMessage_impl(const wchar_t* message);
 
 #ifdef __cplusplus
 }
-#endif
+#endif // __cplusplus
+#endif // __NVDLL_H
