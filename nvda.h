ad84c793f5b0b4118f102129bd6891d1b58bc799
woclass <git@wo-class.cn>
2023-01-23 23:05:22 +0800

    nvda.h NVDA 函数名兼容

---
+++ nvda.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#if !defined(__NVDA_H) && !defined(__nvdaController_h__)
+/// NVDA 函数名兼容
+#define __NVDA_H
+
+
+#define nvdaController_testIfRunning    testIfRunning_impl
+#define nvdaController_speakText        speakText_impl
+#define nvdaController_cancelSpeech     cancelSpeech_impl
+#define nvdaController_brailleMessage   brailleMessage_impl
+
+#endif // __NVDA_H
