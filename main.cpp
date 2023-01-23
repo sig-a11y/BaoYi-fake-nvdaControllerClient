ee218be12d04c89ea40f2cf8d3653ce5f48d2128
woclass <git@wo-class.cn>
2023-01-23 19:54:05 +0800

    添加中文配置项

--- main.cpp
+++ main.cpp
@@ -44,17 +44,17 @@ const LPCWSTR DLL_LOG_NAME = nullptr;
 
 /// 配置文件对象
 static CSimpleIniW ini;
-/// 保益 DLL 配置 ini 文件名
+/// 保益 DLL 配置 ini 文件名. TODO: 使用枚举
 const LPCWSTR INI_NAME = L"朗读配置.ini";
-//const LPCWSTR INI_APP_NAME = L"朗读";
+const LPCWSTR INI_NAME_CN = L"朗读配置.ini";
 const LPCWSTR INI_APP_NAME = L"APP";
-//const LPCWSTR INI_KEY_USE_SLAVE = L"独立通道";
+const LPCWSTR INI_APP_NAME_CN = L"朗读";
 const LPCWSTR INI_KEY_USE_SLAVE = L"USE_SLAVE";
-//const LPCWSTR INI_KEY_USE_APPEND = L"排队朗读";
+const LPCWSTR INI_KEY_USE_SLAVE_CN = L"独立通道";
 const LPCWSTR INI_KEY_USE_APPEND = L"USE_APPEND";
-//const LPCWSTR INI_KEY_ALLOW_BREAK = L"按键打断朗读";
+const LPCWSTR INI_KEY_USE_APPEND_CN = L"排队朗读";
 const LPCWSTR INI_KEY_ALLOW_BREAK = L"ALLOW_BREAK";
-
+const LPCWSTR INI_KEY_ALLOW_BREAK_CN = L"按键打断朗读";
 
 /// DLL 句柄
 static HMODULE dllHandle;
