b764ea3e73241e1b231e934f2734a292491ddd1d
woclass <git@wo-class.cn>
2023-01-23 21:40:00 +0800

    清理 ini 导出函数

--- ini.cpp
+++ ini.cpp
@@ -11,8 +11,26 @@
 namespace ini {
     using boy::logWString;
 
+#pragma region 常量变量定义
+    // TODO: 使用枚举
+
+    /// 保益 DLL 配置 ini 文件名. 
+    LPCWSTR INI_NAME = L"nvdaCfg.ini";
+    LPCWSTR INI_NAME_CN = L"朗读配置.ini";
+
+    LPCWSTR INI_APP_NAME = L"APP";
+    LPCWSTR INI_APP_NAME_CN = L"朗读";
+    LPCWSTR INI_KEY_USE_SLAVE = L"USE_SLAVE";
+    LPCWSTR INI_KEY_USE_SLAVE_CN = L"独立通道";
+    LPCWSTR INI_KEY_USE_APPEND = L"USE_APPEND";
+    LPCWSTR INI_KEY_USE_APPEND_CN = L"排队朗读";
+    LPCWSTR INI_KEY_ALLOW_BREAK = L"ALLOW_BREAK";
+    LPCWSTR INI_KEY_ALLOW_BREAK_CN = L"按键打断朗读";
+
     /// 配置文件对象
     static CSimpleIniW ini;
+#pragma region
+
 
     /// 非0值作为 true
     /**
--- ini.hpp
+++ ini.hpp
@@ -1,25 +1,11 @@
 ﻿#pragma once
 #ifndef __BOY_INI_H
 #define __BOY_INI_H
-#include <wtypes.h>  // LPCWSTR
-
 
 namespace ini {
-    // TODO: 使用枚举
-
-    /// 保益 DLL 配置 ini 文件名. 
-    LPCWSTR INI_NAME = L"nvdaCfg.ini";
-    LPCWSTR INI_NAME_CN = L"朗读配置.ini";
-    LPCWSTR INI_APP_NAME = L"APP";
-    LPCWSTR INI_APP_NAME_CN = L"朗读";
-    LPCWSTR INI_KEY_USE_SLAVE = L"USE_SLAVE";
-    LPCWSTR INI_KEY_USE_SLAVE_CN = L"独立通道";
-    LPCWSTR INI_KEY_USE_APPEND = L"USE_APPEND";
-    LPCWSTR INI_KEY_USE_APPEND_CN = L"排队朗读";
-    LPCWSTR INI_KEY_ALLOW_BREAK = L"ALLOW_BREAK";
-    LPCWSTR INI_KEY_ALLOW_BREAK_CN = L"按键打断朗读";
 
     void loadIni();
+
 } // ini::
 
 #endif // __BOY_INI_H
