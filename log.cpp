b9ad5d8ea7a16d71394b3941c4d53e0889362ef1
woclass <git@wo-class.cn>
2023-01-24 10:25:41 +0800

    初始化 spdlog

--- log.cpp
+++ log.cpp
@@ -1,21 +1,50 @@
 ﻿#include "log.hpp"
 #include "dll.hpp"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace nvdll {
 namespace log {
+
+    constexpr LPCSTR DEBUG_LOG_NAME = "nvdaDll-debug.log";
+    constexpr LPCSTR WARN_LOG_NAME = "nvdaDll.log";
+
+    /// 初始化 spdlog
+    void spdlog_init()
+    {
+        // -- 彩色终端输出
+        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        console_sink->set_level(spdlog::level::info);
+
+        // -- release 模式：日志文件仅输出 warn
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(WARN_LOG_NAME, true);
+        file_sink->set_level(spdlog::level::warn);
+#ifndef _DEBUG
+        spdlog::logger logger("release_sink", { console_sink, file_sink });
+        logger.set_level(spdlog::level::warn);
+#else
+        // -- debug 模式：日志文件输出 warn+debug
+        auto debug_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(DEBUG_LOG_NAME, true);
+        debug_file_sink->set_level(spdlog::level::debug);
+        spdlog::logger logger("debug_sink", { console_sink, file_sink, debug_file_sink });
+        logger.set_level(spdlog::level::debug);
+#endif // ndef _DEBUG
+
+        // ==== 初始化完毕
+        logger.info("spdlog init finished.");
+    }
 
     void init()
     {
 #ifdef _DEBUG
         loguru::add_file("fakeNvda-debug.log", loguru::Truncate, loguru::Verbosity_INFO);
-        auto log = spdlog::basic_logger_mt("basic_logger", "fakeNvda-debug.txt");
-        log->set_level(spdlog::level::debug);
-        spdlog::flush_every(std::chrono::seconds(1));
+        
 #endif // def _DEBUG
         DLOG_F(INFO, "loguru init.");
         DLOG_F(INFO, "BaoYi Dll API Version: %s", dll::boy::BOY_DLL_VERSION);
         DLOG_F(INFO, "Compiled at: %s %s", __DATE__, __TIME__);
+
+        spdlog_init();
     }
 
     /// 通过日志输出 WString 类型的变量
